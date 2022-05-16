67c9df267533ff594459626ead52b2b6c57c38d7
shibez0112 <ginta2888@gmail.com>
2022-05-16 11:21:36 +0700

    update item models, views, controllers, paging

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -1,8 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
 namespace SportsStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private IStoreRepository repository;
+        public int PageSize = 4; /*4 product pages*/
+       
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IActionResult Index(int productPage = 1) 
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                .OrderBy(p => p.ProductID)
+                .Skip((productPage - 1) * PageSize)
+                .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                }
+            });
     }
 }
