3eea7b39e00580538ef960f73c867ff0b51a3f53
shibez0112 <ginta2888@gmail.com>
2022-05-17 10:52:03 +0700

    Add category bar, routing for specific page and category

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -13,10 +13,11 @@ namespace SportsStore.Controllers
         {
             repository = repo;
         }
-        public IActionResult Index(int productPage = 1) 
+        public ViewResult Index(string? category, int productPage = 1) 
             => View(new ProductsListViewModel
             {
                 Products = repository.Products
+                .Where(p => category == null ||p.Category == category)
                 .OrderBy(p => p.ProductID)
                 .Skip((productPage - 1) * PageSize)
                 .Take(PageSize),
@@ -25,7 +26,8 @@ namespace SportsStore.Controllers
                     CurrentPage = productPage,
                     ItemsPerPage = PageSize,
                     TotalItems = repository.Products.Count()
-                }
+                },
+                CurrentCategory = category
             });
     }
 }
