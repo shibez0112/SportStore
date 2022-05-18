32359a3a2808d5a17866c55acc4354cd4d4ddcf0
shibez0112 <ginta2888@gmail.com>
2022-05-18 15:56:08 +0700

    finish routing between category and add user cart

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -12,12 +12,18 @@ builder.Services.AddDbContext<StoreDbContext>(opts => {
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
 
+builder.Services.AddRazorPages();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession();
+
 
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
 
 app.UseStaticFiles();
+app.UseSession();
+
 
 app.MapControllerRoute("catpage",
     "{category}/Page{productPage:int}",
@@ -34,6 +40,8 @@ app.MapControllerRoute("pagination",
     new { Controller = "Home", action = "Index", productPage = 1});
 
 app.MapDefaultControllerRoute();
+
+app.MapRazorPages();
 
 SeedData.EnsurePopulated(app); 
 
